ecb673c3a6c3f4eede8c01502e7cd6a9180c6ad0
SnorreJohnsen <snorrejohnsen@gmail.com>
2026-02-17 11:09:19 +0100

    made lookup table for halow module returning vals for dist_comm_calc

--- mesh_design.py
+++ mesh_design.py
@@ -292,6 +292,74 @@ def shannon(data_rate_Mbps: float,
 
     return received_power_dbm
 
+# Look up table for halow module MM8108-MF15457 values are (data_rate, received_sensitivity, transmit_power) 
+lookup_table_halow_module_MM8108 = {
+    2: {
+        0: {"data_rate": 0.7,  "receive_sensitivity": -103, "transmit_power": 25.0},
+        1: {"data_rate": 1.4, "receive_sensitivity": -101, "transmit_power": 25.0},
+        2: {"data_rate": 2.2,  "receive_sensitivity": -99, "transmit_power": 25.0},
+        3: {"data_rate": 2.9, "receive_sensitivity": -96, "transmit_power": 25.0},
+        4: {"data_rate": 4.3,  "receive_sensitivity": -93, "transmit_power": 24.5},
+        5: {"data_rate": 5.8, "receive_sensitivity": -89, "transmit_power": 23.0},
+        6: {"data_rate": 6.5,  "receive_sensitivity": -87, "transmit_power": 21.5},
+        7: {"data_rate": 7.2, "receive_sensitivity": -86, "transmit_power": 20.0},
+        8: {"data_rate": 8.9,  "receive_sensitivity": -82, "transmit_power": 18.0},
+    },
+
+    4: {
+        0: {"data_rate": 1.5,  "receive_sensitivity": -102, "transmit_power": 22.5},
+        1: {"data_rate": 3.0, "receive_sensitivity": -99, "transmit_power": 22.5},
+        2: {"data_rate": 4.5,  "receive_sensitivity": -97, "transmit_power": 22.5},
+        3: {"data_rate": 6.0, "receive_sensitivity": -94, "transmit_power": 22.5},
+        4: {"data_rate": 9.0,  "receive_sensitivity": -90, "transmit_power": 22.5},
+        5: {"data_rate": 12, "receive_sensitivity": -86, "transmit_power": 21.5},
+        6: {"data_rate": 14,  "receive_sensitivity": -85, "transmit_power": 20.5},
+        7: {"data_rate": 15, "receive_sensitivity": -83, "transmit_power": 19.5},
+        8: {"data_rate": 18,  "receive_sensitivity": -79, "transmit_power": 19.0},
+        9: {"data_rate": 20, "receive_sensitivity": -78, "transmit_power": 17.0},
+    },
+
+    8: {
+        0: {"data_rate": 3.3,  "receive_sensitivity": -98, "transmit_power": 22.5},
+        1: {"data_rate": 6.5, "receive_sensitivity": -95, "transmit_power": 22.0},
+        2: {"data_rate": 9.8,  "receive_sensitivity": -93, "transmit_power": 22.0},
+        3: {"data_rate": 13, "receive_sensitivity": -90, "transmit_power": 22.0},
+        4: {"data_rate": 20,  "receive_sensitivity": -87, "transmit_power": 22.0},
+        5: {"data_rate": 26, "receive_sensitivity": -83, "transmit_power": 21.5},
+        6: {"data_rate": 29,  "receive_sensitivity": -81, "transmit_power": 20.5},
+        7: {"data_rate": 33, "receive_sensitivity": -80, "transmit_power": 20.0},
+        8: {"data_rate": 39,  "receive_sensitivity": -76, "transmit_power": 19.0},
+        9: {"data_rate": 43, "receive_sensitivity": -74, "transmit_power": 16.0},
+    }
+}
+
+def get_halow_module_MM8108_params(desired_bandwidth_Mhz, desired_rate_Mbps):
+    # Find closest available bandwidth
+    available_bandwidth = np.array(list(lookup_table_halow_module_MM8108.keys()))
+    bandwidth_index = np.argmin(np.abs(available_bandwidth - desired_bandwidth_Mhz))
+    closest_bandwidth = int(available_bandwidth[bandwidth_index])
+
+    # Get all MCS schemes for that bandwidth
+    schemes = lookup_table_halow_module_MM8108[closest_bandwidth].values()
+
+    # Find the sorted_scheme with data_rate closest to desired_rate_Mbps
+    sorted_schemes = sorted(schemes, key=lambda s: abs(s['data_rate'] - desired_rate_Mbps))
+
+    # Find first sorted_scheme >= desired datarate
+    for best_scheme in sorted_schemes:
+        if best_scheme['data_rate'] >= desired_rate_Mbps:
+            best_data_rate = best_scheme['data_rate']
+            best_rx_sens = best_scheme['receive_sensitivity']
+            best_tx_power = best_scheme['transmit_power']
+            break
+        else:
+            # If desired data rate above all availeble schemes return highest sorted_scheme
+            best_scheme = sorted_schemes[0]
+            best_data_rate = best_scheme['data_rate']
+            best_rx_sens = best_scheme['receive_sensitivity']
+            best_tx_power = best_scheme['transmit_power']
+
+    return best_data_rate, best_rx_sens, best_tx_power, closest_bandwidth
 
 def dist_comm_calc(transmit_power_dbm: float = 16, 
                    received_power_dbm: float = -74,
@@ -312,7 +380,8 @@ def dist_comm_calc(transmit_power_dbm: float = 16,
     # Calculate free space path loss 
     fspl = transmit_power_dbm + transmit_gain_dbi + received_gain_dbi - received_power_dbm - margin_loss_db
 
-    # Calculate communication distance
+    # Calculate communication distance 32.44 is unit conversion constant  
+    # 32.44 = 20 log10(4pi/c) + 20 log10(10^3) + 20 log10(10^6)
     dist_comm_km = 10**((fspl - 20 * math.log10(freq_Mhz) - 32.44) / 20)
     
     dist_comm = dist_comm_km * 1000
@@ -416,16 +485,14 @@ file_folder = "./mesh_design_out"
 test_tolerances = (0.05, 0.15, 3) #tolerances in percentage (min, max, step)
 test_dist_redundancy = 0
 
-# These variables is chosen from datasheet 
-# BW = ...  
-# Mod scheme = .... 
-# datarate_scheme = 
-transmit_power_dbm = 21.5
-received_power_dbm = -83
+desired_bandwidth_Mhz = 4
+desired_rate_Mbps = 13
 freq_Mhz = 868
 margin_loss_db = 3
 ###################################################################
 
+data_rate, received_power_dbm, transmit_power_dbm, bandwidth = get_halow_module_MM8108_params(desired_bandwidth_Mhz=desired_bandwidth_Mhz, desired_rate_Mbps=desired_rate_Mbps)
+print(f"{data_rate=},{received_power_dbm=}, {transmit_power_dbm=}, {bandwidth=}")
 
 dist_comm = dist_comm_calc(transmit_power_dbm=transmit_power_dbm, 
                            received_power_dbm=received_power_dbm, 
