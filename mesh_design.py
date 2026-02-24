6ba1d83b671b420ece33731b908907afc05460dd
SnorreJohnsen <snorrejohnsen@gmail.com>
2026-02-24 08:50:14 +0100

    added some expleining comments to test variables section

--- mesh_design.py
+++ mesh_design.py
@@ -924,25 +924,28 @@ def main():
     ###############################################################################
     #__________________________ TEST PARAMETERS __________________________________#
     ###############################################################################
+    # dimensions of area
     length = 30000
     width = 10000
     scale_factor = 1
-
     test_dim = (length*scale_factor, width*scale_factor)
-    test_samples = (600, 200)
+    test_samples = (600, 200)                           # number of sample points on area (x, y)
 
-    test_tolerances = np.arange(100, 300, 100)       #tolerance in meters (min, max, stepsize) 
-    test_dist_redundancy = 0          
-    test_dropout_rates = np.arange(0.1, 0.3, 0.1)    #dropout rate in percentage (min, max, stepsize)
-    test_dropout_iters = 100                         # number of iterations for each dropout rate (used for histogram)
+    test_tolerances = np.arange(100, 300, 100)          #tolerance in meters (min, max, stepsize) 
+    test_dist_redundancy = 0                         # distance redundancy for drone placement
+    test_dropout_rates = np.arange(0.1, 0.3, 0.1)      #dropout rate in percentage (min, max, stepsize)
+    test_dropout_iters = 100                            # number of iterations for each dropout rate (used for histogram)
     
+    # wireless communication parameters for MM8108-MF15457 lookup table
     wireless_prefix = ""
     desired_bandwidth_Mhz = 8
     desired_rate_Mbps = 20
     freq_Mhz = 868
-    margin_loss_db = 3
+    margin_loss_db = 3                          # safety variable for "other" losses
 
     # Set grid type to process
+    # if hexagonal grid is chosen bool variable extra_edge_drones 
+    # has to be set in function process_drone_mesh
     test_grid_meta_prefix = "SQUARE"
     test_grid_func = drone_sq_grid
     ###############################################################################
