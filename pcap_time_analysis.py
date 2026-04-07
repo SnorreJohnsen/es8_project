01740610641fec604231aa0b19d45157f15c7046
Mathias Joergensen <mathiasjoergensen@hotmail.com>
2026-04-07 11:02:54 +0200

    adds txt files for time analysis of emulation_output and emulation_output_no_netem

--- pcap_time_analysis.py
+++ pcap_time_analysis.py
@@ -31,7 +31,8 @@ def describe(data, f=None):
 
 def hist_box(diffs: list,
              stats_folder: Path,
-             num_packets: int):
+             num_packets: int,
+             pcap_name: str):
 
     # ---- Save Histogram for this iteration ----
     plt.figure(figsize=(8,5))
@@ -40,8 +41,9 @@ def hist_box(diffs: list,
     plt.ylabel('Count')
     plt.title(f'Histogram for {num_packets} packets')
     plt.grid(True, linestyle='--', alpha=0.5)
-    hist_path = stats_folder / f"time_difference_histogram_{num_packets}_packets.png"
+    hist_path = stats_folder / f"{pcap_name}_histogram_{num_packets}_packets.png"
     plt.savefig(hist_path)
+    print(f'Histograms saved to {hist_path}')
     plt.close()
 
     # ---- Save Boxplot for this iteration ----
@@ -50,8 +52,9 @@ def hist_box(diffs: list,
     plt.ylabel('Time Difference (s)')
     plt.title(f'Boxplot for {num_packets} packets')
     plt.grid(True, linestyle='--', alpha=0.5)
-    boxplot_path = stats_folder / f"time_difference_boxplot_{num_packets}_packets.png"
+    boxplot_path = stats_folder / f"{pcap_name}_boxplot_{num_packets}_packets.png"
     plt.savefig(boxplot_path)
+    print(f'Boxplots saved to {boxplot_path}')
     plt.close()
 
 # Helper to get total number of packets in the file
@@ -117,19 +120,24 @@ def max_diff_same_frame(pcap_file,
 if __name__ == "__main__":
 
     # Replace with your PCAP path
-    pcap_file = r"C:\UNI\8.Semester\Project\pcap_test_files\merged_smus_test.pcap"
+    #pcap_file = r"C:\UNI\8.Semester\Project\emulation_output\pcaps\merged_cleaned_10u.pcap" 
+    pcap_file = r"C:\UNI\8.Semester\Project\emulation_output_no_netem\pcaps\merged_cleaned_no_netem_10u.pcap" 
+    #pcap_file = r"C:\UNI\8.Semester\Project\pcap_test_files\merged_smus_test.pcap"
+    pcap_name = Path(pcap_file).stem # keep for file name
 
     # Define folder and filename for .txt
     stats_folder = Path(__file__).parent / "pcap_time_diff_stats"
     stats_folder.mkdir(parents=True, exist_ok=True)
 
-    txt_path = stats_folder / "time_difference_pcap.txt"
-    hist_path = stats_folder / "time_difference_histogram.png"
-    boxplot_path = stats_folder / "time_difference_boxplot.png"
+    # Path for txt file
+    txt_path = stats_folder / f"{pcap_name}_time_analysis.txt"
 
     # Get total packets and desired packets
     total_packets = get_packet_count(pcap_file=pcap_file)
-    desired_packets = [1_000, 10_000, 100_000, 1_000_000, total_packets]
+    desired_packets = sorted(set(
+        min(x, total_packets)
+        for x in [1_000, 10_000, 100_000, 1_000_000, total_packets]))
+    print(f'{desired_packets=}')
  
     # Save txt file
     with open(txt_path, "w") as f:
@@ -149,8 +157,7 @@ if __name__ == "__main__":
 
             hist_box(diffs=diffs,
                      stats_folder=stats_folder,
-                     num_packets=packet)
+                     num_packets=packet,
+                     pcap_name=pcap_name)
 
     print(f'Results saved to {txt_path}')
-    print(f'Histograms saved to {hist_path}')
-    print(f'Boxplots saved to {boxplot_path}')
