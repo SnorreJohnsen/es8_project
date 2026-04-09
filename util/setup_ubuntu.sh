5e8920e08b94cc91ce28e41999534ceecc923080
mhtoribio <23717111+mhtoribio@users.noreply.github.com>
2026-04-09 11:00:17 +0200

    add setup script

---
+++ util/setup_ubuntu.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -eu
+
+# Must be run from repo root
+if [ ! -f .gitignore ] || [ ! -f .gitmodules ] || [ ! -f README.md ]; then
+	echo "Setup must be run from repo root."
+	echo "Failed finding .gitignore, .gitmodules and README.md."
+	echo "$PWD does not look like the repo root."
+	exit
+fi
+
+sudo apt update
+sudo apt install -y \
+	build-essential pkg-config libnl-3-dev libnl-genl-3-dev \
+	python3 python3-venv \
+	batctl iperf3
+
+echo "[*] Creating venv"
+python3 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+
+echo "[*] Building patched batman kernel module"
+cd batman-patch/batman-adv
+make -j "$(nproc)"
+sudo make install
+
+echo "[*] Building battpctl"
+cd ../battpctl
+make
+cd ../..
+ln -s ../../batman-patch/battpctl/battpctl .venv/bin/battpctl # put battpctl in PATH; an ugly hack but works for our use case
+
+echo "[*] Done"
